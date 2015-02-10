f7427ddbee07dbb4a57ab40f6d0bc790ecaef334
Scully87 <scully87.ts@googlemail.com>
2015-02-10 22:48:09 +0000

    assign new value to variable

--- Two/helloworld.js
+++ Two/helloworld.js
@@ -1,8 +1,10 @@
 var myVariable; //undefined variable declaration statement
 myVariable = "Hello World!"; //variable initialization statement
-
 alert(myVariable)
 
 var yourVariable = "Hello You!"; // variable declaration AND initialization statement
-
 alert(yourVariable)
+
+var herVariable = "Hello Her!";
+herVariable = "Hello Him!"; // you can assign new value to variable
+alert(herVariable)
