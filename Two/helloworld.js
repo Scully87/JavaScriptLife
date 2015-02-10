05bb48b0897f196304c8c203e48c02a62df56bfe
Scully87 <scully87.ts@googlemail.com>
2015-02-10 22:50:59 +0000

    other data types

--- Two/helloworld.js
+++ Two/helloworld.js
@@ -8,3 +8,7 @@ alert(yourVariable)
 var herVariable = "Hello Her!";
 herVariable = "Hello Him!"; // you can assign new value to variable
 alert(herVariable)
+
+var ten = 10; //number
+var pi = 3.14; //fraction
+var isBoolean = true //true or false
