99e4de9b5b6f3d1fd2ca644587a79c2cc307b530
Scully87 <scully87.ts@googlemail.com>
2015-02-10 23:03:19 +0000

    multiple variables

--- Three/script.js
+++ Three/script.js
@@ -1,2 +1,8 @@
-var sum = 4 + 5; // '=' is assignment operator
-                 // '+' operator
+// '=' is assignment operator
+// '+' operator
+// use comma to define multiple variables in same statement
+
+var sum = 4 + 5, 
+    difference = 4 - 5,
+    product = 4 * 5;
+
