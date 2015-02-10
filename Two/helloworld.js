f0903ae0b664540a466df58f0b42b69bad0d47fc
Scully87 <scully87.ts@googlemail.com>
2015-02-10 22:44:06 +0000

    variable declared and initialzed

--- Two/helloworld.js
+++ Two/helloworld.js
@@ -2,3 +2,7 @@ var myVariable; //undefined variable declaration statement
 myVariable = "Hello World!"; //variable initialization statement
 
 alert(myVariable)
+
+var yourVariable = "Hello You!"; // variable declaration AND initialization statement
+
+alert(yourVariable)
