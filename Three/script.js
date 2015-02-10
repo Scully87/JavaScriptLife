d6bfd20998548a44ab37b4e348fe1871db586f31
Scully87 <scully87.ts@googlemail.com>
2015-02-10 23:07:55 +0000

    maths variable added to new math

--- Three/script.js
+++ Three/script.js
@@ -4,5 +4,10 @@
 
 var sum = 4 + 5, 
     difference = 4 - 5,
-    product = 4 * 5;
+    product = 4 * 5,
+    quotient = 4 / 5;
+
+// works in operation order, times, addition, subtract
+var foo = 4 + 5 * (6 - 8);
+    bar = foo + 1 // adding variable value and adding to it, assigning that value to a new variable
 
