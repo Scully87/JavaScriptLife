46fde187697a6870fc682a7fdc1a369c277562aa
Scully87 <scully87.ts@googlemail.com>
2015-02-11 09:45:47 +0000

    annonymous function

--- Four/script.js
+++ Four/script.js
@@ -7,3 +7,36 @@ foo = foo * 8; //48     modifying the value of the variable
 var bar = 3 + 3; //6
 bar = bar + 1;  //7   modifying the value of the variable
 bar = bar * 8;  //56  modifying the value of the variable
+
+
+function doSomething(){     // empty parenthatsisdoes not accept external data
+                            // function body inside the curly braces
+}
+
+function doSomething(paramOne, paramTwo){     // parameter(special variable for function) in parenthatsis, empty does not accept external data
+    paramOne = paramOne + 3        // function body inside the curly braces
+    paramOne = paramOne + 1
+    paramOne = paramOne * 8
+
+    return paramOne * paramTwo  // must return the parameter to be viewed
+}
+
+var poo = doSomething(2, 2) // using function with the paramaters
+var tar = doSomething(3, 3)
+
+alert(poo)
+alert(tar)
+
+
+// annonymous function as a variable
+// this code will not run unless it is put to the top of the script!
+var doSomething1 = function(paramOne, paramTwo){     // parameter(special variable for function) in parenthatsis, empty does not accept external data
+    paramOne = paramOne + 3        // function body inside the curly braces
+    paramOne = paramOne + 1
+    paramOne = paramOne * 8
+
+    return paramOne * paramTwo  // must return the parameter to be viewed
+
+    var poo = doSomething1(2, 2) // using function with the paramaters
+    var tar = doSomething1(3, 3)
+}
