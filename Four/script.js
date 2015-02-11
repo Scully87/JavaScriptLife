ea06bd4ba2c5aeae2827d42ac7a2758f0dafebd4
Scully87 <scully87.ts@googlemail.com>
2015-02-11 09:12:52 +0000

    modifying variables

---
+++ Four/script.js
@@ -0,0 +1,9 @@
+// set one
+var foo = 2 + 3; //5
+foo = foo + 1; //6      modifying the value of the variable
+foo = foo * 8; //48     modifying the value of the variable
+
+//set two
+var bar = 3 + 3; //6
+bar = bar + 1;  //7   modifying the value of the variable
+bar = bar * 8;  //56  modifying the value of the variable
