4d162898f95da7ca60512d69a3e890efe1ceed51
Scully87 <scully87.ts@googlemail.com>
2015-02-11 09:09:37 +0000

    parseInts and parseFloats

--- Three/script.js
+++ Three/script.js
@@ -14,3 +14,17 @@ var foo = 4 + 5 * (6 - 8);
 var turd = "Hello" + "World!" // concatination operator
 
 var balls = "6" + (4 * 3) //612 concatinates next string and adds product to string
+
+var foo = 6 + ""; // converting number to string
+
+var bar = parseInt("34"); //converting string to number
+
+var bar = parseInt("34sdsad54", 10); //converting string to just takes integers
+
+var bar = parseInt("s"); //Nan
+
+var bar = parseInt(3.14); // parse float into integer ...3
+
+var bar = parseFloat("3.14"); // parse string to float
+
+
